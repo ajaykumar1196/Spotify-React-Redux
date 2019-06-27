bebc1da650771ad8da32958e77c43cf584d3f88d
ajaykumar1196 <ajaykumar1196@gmail.com>
2019-06-27 09:31:06 +0530

    Added scropes contant for spotify login

---
+++ src/constants/authConstants.js
@@ -0,0 +1,20 @@
+export const SCOPES = [
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'streaming',
+  'ugc-image-upload',
+  'user-follow-modify',
+  'user-follow-read',
+  'user-library-read',
+  'user-library-modify',
+  'user-read-private',
+  'user-read-birthdate',
+  'user-read-email',
+  'user-top-read',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'user-read-recently-played'
+];
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,48 +1,24 @@
-
+import { SCOPES } from '../constants/authConstants'
 export default class AuthService {
-  static get allScopes() {
-    return [
-      'playlist-read-private',
-      'playlist-read-collaborative',
-      'playlist-modify-public',
-      'playlist-modify-private',
-      'streaming',
-      'ugc-image-upload',
-      'user-follow-modify',
-      'user-follow-read',
-      'user-library-read',
-      'user-library-modify',
-      'user-read-private',
-      'user-read-birthdate',
-      'user-read-email',
-      'user-top-read',
-      'user-read-playback-state',
-      'user-modify-playback-state',
-      'user-read-currently-playing',
-      'user-read-recently-played'
-    ];
-  }
-
   static setAuthToken = (token) => {
-    localStorage.setItem('_auth_token', token)
+    localStorage.setItem('auth_token', token)
   }
   static getAuthToken = () => {
-    return localStorage.getItem('_auth_token')
+    return localStorage.getItem('auth_token')
   }
   static isAuthenticated = () => {
     return this.getAuthToken() !== null
   }
   static logout = () => {
-    localStorage.removeItem('_auth_token')
+    localStorage.removeItem('auth_token')
   }
 
   static login = () => {
-    console.log(process.env.REACT_APP_CLIENT_ID)
     let queryArguments = [];
     queryArguments.push(`client_id=${process.env.REACT_APP_CLIENT_ID}`);
     queryArguments.push(`response_type=token`);
     queryArguments.push(`redirect_uri=${encodeURIComponent(`${window.location.protocol}//${window.location.host}${window.location.pathname}callback`)}`);
-    queryArguments.push(`scope=${this.allScopes.join(' ')}`);
+    queryArguments.push(`scope=${SCOPES.join(' ')}`);
     return `https://accounts.spotify.com/authorize/?${queryArguments.join('&')}`;
 
   }
