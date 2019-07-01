ad8111a138c8abea501be3d8a1c89124ec544320
Ajay Kumar <ajaykumar1196@gmail.com>
2019-07-01 23:18:59 +0530

    Added mediaObject and mediaContainer

---
+++ src/components/MediaContainer/MediaContainer.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./MediaContainer.css";
+import MediaObject from "../MediaObject/MediaObject";
+
+class MediaContainer extends React.Component {
+  renderMediaObjects = ({ items, type }) => {
+    return items.map(item => {
+      const pathName = type + "/" + item.id;
+      const artwork = item.images ? item.images[0].url : item.icons[0].url;
+      return <MediaObject artwork={artwork} link={pathName} name={item.name} />;
+    });
+  };
+
+  render() {
+    return (
+      <div className="mediaContainer">
+        <div>
+          <h1 className="mediaContainer-heading">{this.props.heading}</h1>
+        </div>
+        <div className="mediaContainer-content">
+          <div className="mediaContainer-content-wrapper">
+            {this.renderMediaObjects(this.props)}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MediaContainer;
---
+++ src/components/MediaObject/MediaObject.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./MediaObject.css";
+class MediaObject extends React.Component {
+  render() {
+    return (
+      <div className="media-object">
+        <div className="media-object-container" style={{ maxWidth: "200px" }}>
+          <div className="media-object-hoverable">
+            <div className="media-object-wrapper ">
+              <a
+                className="media-object-cover-art actionable shadow"
+                href={this.props.link}
+                style={{ width: "auto", height: "auto" }}
+              >
+                <div>
+                  <div className="icon">
+                    <svg
+                      width="55"
+                      height="55"
+                      viewBox="0 0 80 81"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <title>Playlist Icon</title>
+                      <path
+                        d="M25.6 11.565v45.38c-2.643-3.27-6.68-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4 14.4-6.46 14.4-14.4v-51.82l48-10.205V47.2c-2.642-3.27-6.678-5.37-11.2-5.37-7.94 0-14.4 6.46-14.4 14.4s6.46 14.4 14.4 14.4S80 64.17 80 56.23V0L25.6 11.565zm-11.2 65.61c-6.176 0-11.2-5.025-11.2-11.2 0-6.177 5.024-11.2 11.2-11.2 6.176 0 11.2 5.023 11.2 11.2 0 6.174-5.026 11.2-11.2 11.2zm51.2-9.745c-6.176 0-11.2-5.024-11.2-11.2 0-6.174 5.024-11.2 11.2-11.2 6.176 0 11.2 5.026 11.2 11.2 0 6.178-5.026 11.2-11.2 11.2z"
+                        fill="#ffffff"
+                        fillRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                  <div
+                    className="media-object-cover-art-image"
+                    style={{
+                      backgroundImage: `url(${this.props.artwork})`
+                    }}
+                  />
+                </div>
+              </a>
+            </div>
+            <div className="mo-info">
+              <div className="react-contextmenu-wrapper">
+                <a
+                  className="mo-info-name"
+                  title="New Music Friday"
+                  dir="auto"
+                  href={this.props.link}
+                >
+                  {this.props.name}
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MediaObject;
