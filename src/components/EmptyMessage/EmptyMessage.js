01d0b502ec483b36d69ee91e78baba7ae4985f01
ajaykumar1196 <ajaykumar1196@gmail.com>
2019-07-12 12:23:39 +0530

    Added EmptyMessage component for empty response list items.

---
+++ src/components/EmptyMessage/EmptyMessage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+import "./EmptyMessage.css";
+import "circular-std";
+
+class EmptyMessage extends React.Component {
+  render() {
+    return (
+      <div className="empty-message">
+        <h1 className="empty-message__title">{this.props.title}</h1>
+        <h4 className="empty-message__subtitle">{this.props.subtitle}</h4>
+        <Link className="btn btn-black cta-button" to="/browse">DISCOVER</Link>
+      </div>
+    )
+  }
+}
+
+export default EmptyMessage;
