f3ca0a31b2d4cbab900efe3ff7282c08728bb8ba
ajaykumar <ajaykumar1196@gmail.com>
2020-07-12 13:39:59 +0530

    login page changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import AuthService from '../../services/authService'
 import AuthCallback from '../AuthCallback/AuthCallback'
+import './Login.css'
 class Login extends React.Component {
   renderLanding() {
     return (
-      <div>
-        <div>
-          <h1>Login with spotify</h1>
-          <a href={AuthService.login()}>Log-in</a>
+      <div className="wrapper">
+        <div className="center-login">
+          <h2>LOGIN WITH SPOTIFY</h2>
+          <a className="btn btn-login cta-button" href={AuthService.login()}>Log-in</a>
         </div>
       </div>
     );
